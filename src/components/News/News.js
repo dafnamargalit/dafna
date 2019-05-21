1fc1712f2660d7b6af9197a06ca3ae4b8e929bec
Dafna <dama7429@colorado.edu>
2019-05-21 16:38:31 -0600

    News

---
+++ src/components/News/News.js
@@ -0,0 +1,19 @@
+import React from 'react';	
+	import Navbar from '../Navbar';
+	import Footer from '../Footer';
+	import './News.css';
+	
+	const News = () => {
+	return(
+	<div className="News">
+	<Navbar/>
+	<div className="News-Content">
+	This page is under construction.
+	</div>
+	<div className="News-Footer">
+	<Footer />
+	</div>
+	</div>
+	);
+	}
+	export default News;
