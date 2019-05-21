079e52807c22a13a773222d116ca4de9bbff6ea6
Dafna <dama7429@colorado.edu>
2019-05-21 16:34:20 -0600

    Ask

---
+++ src/components/Ask/Ask.js
@@ -0,0 +1,20 @@
+import React from 'react';	
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import './Ask.css';
+	
+const Ask = () => {
+	return(
+	<div className="Ask">
+	<Navbar/>
+	<div className="Ask-Content">
+	This page is under construction.
+	</div>
+	<div className="Ask-Footer">
+	<Footer />
+	</div>
+	</div>
+	);
+	}
+
+export default Ask;
