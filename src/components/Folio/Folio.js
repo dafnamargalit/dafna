1ea56e46ec46cfb6a2f5b12408b483f6203fdf3e
Dafna <dama7429@colorado.edu>
2019-05-22 22:40:47 -0600

    folio music

--- src/components/Folio/Folio.js
+++ src/components/Folio/Folio.js
@@ -1,5 +1,4 @@
 import React from 'react';	
-	import ReactPlayer from 'react-player';
 	import Navbar from '../Navbar';
 	import Footer from '../Footer';
 	import F from '../images/F.png';
@@ -8,10 +7,12 @@ import React from 'react';
 	import I from './I.png';
 	import apostrophe from './apostrophe.png';
 	import portfolio from './port.png';
-	
+	import record from './record_player.gif';
+
 	import '../Ask/Ask.css';
 	import './Folio.css';
 	const Folio = () => {
+
 	return(
 	<div className="Folio">
 	<div className="Folio-Navbar">
@@ -32,11 +33,9 @@ import React from 'react';
 	</div>
 	<img src={portfolio} className="Folio-parenth" alt="portfolio"/>
 	<div className="Folio-Content">
-	<div className="video">
-	<ReactPlayer>
-	
-	</ReactPlayer>
-	</div>
+	<a href="https://fanlink.to/dafna">
+	<img className="video" src={record} alt="record player" ></img>
+	</a>
 	{/* <p id="Folio-Text"><a> PROJECTS <br></br></a></p>
 	<p id="Folio-Text"><a> RESUME <br></br></a></p>*/}
 	</div>
