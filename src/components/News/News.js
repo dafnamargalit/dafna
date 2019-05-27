6d8fc3315de30ac8bd78a2e3c94c9caa9bf81692
Dafna <dama7429@colorado.edu>
2019-05-27 10:20:09 -0700

    Home footer

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,15 +1,25 @@
 import React from 'react';	
 	import Navbar from '../Navbar';
 	import Footer from '../Footer';
+	import N from './N.png';
+	import E from './E.png';
+	import W from './W.png';
+	import S from './S.png';
+	import album from '../Landing/spirit.jpg';
+
 	import './News.css';
-	
 	const News = () => {
 	return(
 	<div className="News">
 	<Navbar/>
-	<div className="News-Content">
-	This page is under construction.
+	<div className="News-Header">
+		<img src={N} className='News-Letters-N' alt="N"/>
+		<img src={E} className='News-Letters-E' alt="E"/>
+		<img src={W} className='News-Letters-W' alt="W"/>
+		<img src={S} className='News-Letters-S' alt="S"/>
 	</div>
+	This page is under construction.
+	
 	<div className="News-Footer">
 	<Footer />
 	</div>
