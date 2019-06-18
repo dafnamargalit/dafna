f46c42666d3fd2b608a3f7d1112ea7f82b60373a
Dafna <dama7429@colorado.edu>
2019-06-18 13:39:04 +0300

    folio components

---
+++ src/components/Merch/Merch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Merch.css'
+
+const Merch = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Merch;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Projects.css'
+
+const Projects = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Projects;
