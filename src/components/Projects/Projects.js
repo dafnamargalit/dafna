7615c75ffc04bc10a4c39c2168f1fc592b943f62
Dafna <dama7429@colorado.edu>
2019-06-24 22:51:12 +0300

    projects desktop

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import projects from '../media/gifs/projects.gif';
 import fish from '../media/images/go-fish1.jpg';
-
+import fmn from '../media/images/forgetmenot1.jpg';
+import btt from '../media/images/battleship.jpg';
+import click from '../media/writing/clik.png';
 import './Projects.css'
 
 const Projects = () => {
     return(
-        <div className="Merch">
+        <div className="Page">
         <div className="Merch-Header">
         <Link to="/folio">
         <div className="container">
@@ -30,27 +32,47 @@ const Projects = () => {
                     <br></br>
                     Go Fish 
                     <br></br>
-                    Language: Python
+                    Language: 
+                    <br></br>
+                    Python
                 </div>
             </div>
         </a>
        
         </div>
         <div className="container">
-        <a href="https://github.com/dafnamargalit/go-fish">
-        <img className="Merch-Video" src={fish} alt="fish" ></img>
+        <a href="https://github.com/dafnamargalit/forgetmenot">
+        <img className="Merch-Video" src={fmn} alt="fmn" ></img>
         <div className="overlay">
                 <div className="description">
                     Project Name: 
                     <br></br>
-                    Go Fish 
+                    Forget Me Not
                     <br></br>
-                    Language: Python
+                    Language: JavaScript
                 </div>
             </div>
         </a>
        
         </div>
+        <div className="container">
+        <a href="https://github.com/dafnamargalit/battleship">
+        <img className="Merch-Video" src={btt} alt="btt" ></img>
+        <div className="overlay">
+                <div className="description">
+                    Project Name: 
+                    <br></br>
+                    Battleship
+                    <br></br>
+                    Language: C
+                </div>
+            </div>
+        </a>
+       
+        </div>
+        </div>
+        <div className="projects-click">
+            <img className="pick" src={click} alt="click"></img>
         </div>
         </div>
     )
