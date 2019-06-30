58ad0d6e14ba2dca66bc81f44879748260e78406
Dafna <dama7429@colorado.edu>
2019-06-30 19:41:21 +0300

    mobile version for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,7 @@ import './Projects.css'
 const Projects = () => {
     return(
         <div className="Page">
+        <div className="desktop">
         <div className="Merch-Header">
         <Link to="/folio">
         <div className="container">
@@ -86,6 +87,76 @@ const Projects = () => {
         </div>
         <div className="projects-click">
             <img className="pick" src={click} alt="click"></img>
+        </div>
+        </div>
+       
+        <div className="mobile">
+        <div className="Projects">
+
+        <Link to="/folio">
+        <div className="container">
+        <img className="projects-video" src={projects} alt="projects" ></img>
+        <div className="projects-overlay">
+                <div className="description">
+                    Back to 'Folio
+                </div>
+            </div>
+        </div>
+        </Link>
+
+        <div className="container">
+        
+        <a href="https://github.com/dafnamargalit/go-fish">
+        <img className="projects-video" src={fish} alt="fish" ></img>
+        <div className="projects-overlay">
+                <div className="description">
+                Project: Go Fish
+                    Language: 
+                    Python
+                </div>
+            </div>
+        </a>
+        </div>
+
+        <div className="container">
+        <a href="https://github.com/dafnamargalit/battleship">
+        <img className="projects-video" src={btt} alt="btt" ></img>
+        <div className="projects-overlay">
+                <div className="description">
+                Project: Battleship
+                    Language: C
+                </div>
+            </div>
+        </a>
+       
+        </div>
+
+        <div className="container">
+        <a href="https://github.com/dafnamargalit/forgetmenot">
+        <img className="projects-video" src={fmn} alt="fmn" ></img>
+        <div className="projects-overlay">
+                <div className="description">
+                Project: ForgetMeNot IOS
+                    Language: JavaScript
+                </div>
+            </div>
+        </a>
+        </div>
+
+        <div className="container">
+        <a href="https://github.com/dafnamargalit/guitarduino-tuner">
+        <img className="projects-video" src={guitarduino} alt="guitarduino" ></img>
+        <div className="projects-overlay">
+                <div className="description">
+                Project: Guitarduino Tuner
+                    Language: C
+                </div>
+        </div>
+        </a>
+    
+       
+        </div>
+        </div>
         </div>
         </div>
     )
