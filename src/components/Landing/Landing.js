39f82a20bf318004792d42c4fbf339ccc79537c5
Dafna <dama7429@colorado.edu>
2020-06-11 18:47:32 -0600

    updated landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,34 +1,54 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+ import {Link} from 'react-router-dom';
 
-import submerge from '../media/album-covers/header4.jpg';
-
+// import background from "../media/album-covers/header4.jpg";
 import './Landing.css';
 
 const Landing = () => {
         return (
-            <div className="Landing">
-            <div className="desktop">
-                <img src={submerge} className="landing-image" alt="album"></img>
-                <div className = "centered-text">
-                    <div className = "text-type">
-                    easy <br></br> 
-                    <a className="text-link" href="https://distrokid.com/hyperfollow/dafna/easy">
-                    6.12.2020
-                    </a>
-                    <br></br>
+            <div>
+                <div className="Landing-Desktop">
+                    <div className="announcement-desktop">
+                        easy
+                        <div id="out-desktop">
+                            out now
+                        </div>
                     </div>
-                 
+                    <div className="buttons-desktop">
+                        <a href="https://dafna.fanlink.to/easy" id="listen" className="landing-button-desktop">
+                            listen now
+                        </a>
+                        <a href="https://linktr.ee/btfacollective" id="donate" className="landing-button-desktop">
+                            donate to BTFA
+                        </a>
+                        <Link to="/Home" className="landing-button-desktop">
+                            continue to website
+                        </Link>
+                    </div>
                 </div>
-               
-                <Link to="/home" className="landing-button">
-                    Continue To Website
-                </Link>
+                <div className="Landing-Mobile">
+                    <div className="announcement-mobile">
+                        easy
+                        <div id="out-mobile">
+                            out now
+                        </div>
+                    </div>
+                    <div className="buttons-mobile">
+                        <a href="https://dafna.fanlink.to/easy" id="listen" className="landing-button-mobile">
+                            listen now
+                        </a>
+                        <a href="https://linktr.ee/btfacollective" id="donate" className="landing-button-mobile">
+                            donate to BTFA
+                        </a>
+                        <Link to="/Home" className="landing-button-mobile">
+                            continue to website
+                        </Link>
+                    </div>
+                </div>
             </div>
+
             
 
-           
-            </div>
         );
     }
 export default Landing;
