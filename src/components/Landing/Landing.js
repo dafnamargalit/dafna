87b3f9e83876394306ed4240a6f7111e687c4bdd
Dafna <dama7429@colorado.edu>
2020-07-15 16:36:39 -0600

    landing page update

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,14 +9,14 @@ const Landing = () => {
             <div>
                 <div className="Landing-Desktop">
                     <div className="announcement-desktop">
-                        easy
+                        yours someday
                         <div id="out-desktop">
-                            out now
+                            7.17.2020
                         </div>
                     </div>
                     <div className="buttons-desktop">
-                        <a href="https://dafna.fanlink.to/easy" id="listen" className="landing-button-desktop">
-                            listen now
+                        <a href="https://distrokid.com/hyperfollow/dafna/yours-someday-3" id="listen" className="landing-button-desktop">
+                            pre-save now
                         </a>
                         <a href="https://linktr.ee/btfacollective" id="donate" className="landing-button-desktop">
                             donate to BTFA
@@ -28,14 +28,14 @@ const Landing = () => {
                 </div>
                 <div className="Landing-Mobile">
                     <div className="announcement-mobile">
-                        easy
+                        yours someday
                         <div id="out-mobile">
-                            out now
+                           7.17.2020
                         </div>
                     </div>
                     <div className="buttons-mobile">
-                        <a href="https://dafna.fanlink.to/easy" id="listen" className="landing-button-mobile">
-                            listen now
+                        <a href="https://distrokid.com/hyperfollow/dafna/yours-someday-3" id="listen" className="landing-button-mobile">
+                            pre-save now
                         </a>
                         <a href="https://linktr.ee/btfacollective" id="donate" className="landing-button-mobile">
                             donate to BTFA
